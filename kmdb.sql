188e50da1cd0cacacdd48eb96e768aca54d27cf6
Marko Jelasity <marko.jelasity@kellogg.northwestern.edu>
2025-01-26 05:03:55 +0000

    Data inserted

--- kmdb.sql
+++ kmdb.sql
@@ -112,13 +112,89 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE movie;
+DROP TABLE studio;
+DROP TABLE actor;
+DROP TABLE role;
+
 
 -- Create new tables, according to your domain model
--- TODO!
+CREATE TABLE movie(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    mpaa_rating TEXT,
+    studio_id INTEGER
+);
+
+CREATE TABLE studio(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE actor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT, 
+    last_name TEXT
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_id INTEGER,
+    actor_id INTEGER,
+    role TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+INSERT INTO movie (title, year_released, mpaa_rating, studio_id)
+VALUES
+('Batman Begins', 2005, 'PG-13', 1),
+('The Dark Knight', 2008, 'PG-13', 1),
+('The Dark Knight Rises', 2012, 'PG-13', 1);
+
+INSERT INTO studio (name)
+VALUES
+('Warner Bros.'),
+('Universal Studios'),
+('20th Century Fox')
+;
+
+INSERT INTO actor (first_name, last_name)
+VALUES
+('Christian', 'Bale'),
+('Michael', 'Caine'),
+('Liam', 'Neeson'),
+('Katie', 'Holmes'),
+('Gary', 'Oldman'),
+('Heath', 'Ledger'),
+('Aaron', 'Eckhart'),
+('Maggie', 'Gyllenhaal'),
+('Tom', 'Hardy'),
+('Joseph', 'Gordon-Levitt'),
+('Anne', 'Hathaway')
+;
+
+INSERT INTO role (movie_id, actor_id, role)
+VALUES
+(1, 1, 'Bruce Wayne'),
+(1, 2, 'Alfred'),
+(1, 3, 'Ra''s Al Ghul'),
+(1, 4, 'Rachel Dawes'),
+(1, 5, 'Commissioner Gordon'),
+(2, 1, 'Bruce Wayne'),
+(2, 6, 'Joker'),
+(2, 7, 'Harvey Dent'),
+(2, 2, 'Alfred'),
+(2, 4, 'Rachel Dawes'),
+(3, 1, 'Bruce Wayne'),
+(3, 5, 'Commissioner Gordon'),
+(3, 9, 'Bane'),
+(3, 10, 'John Blake'),
+(3, 11, 'Selina Kyle')
+;
+
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -126,7 +202,9 @@
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+SELECT title, year_released, mpaa_rating 
+FROM movie
+;
 
 -- Prints a header for the cast output
 .print ""
